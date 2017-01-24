62cc438a6a19e712e4552ab857d487124deb31f4
armouse <admin@proger.org>
2017-01-24 14:16:04 +0300

    STUDY-17 - __init__

--- g25.py
+++ g25.py
@@ -1,16 +1,25 @@
+# class C2: pass
+# class C3: pass
+#
+# class C1(C2, C3):
+#     def setname(self, who):
+#         self.name = who
+#
+# user1 = C1()
+# user2 = C1()
+#
+#
+# user1.setname('Станислав')
+# user2.setname('Савик')
+#
+# print(user1.name)
+# print(user2.name)
+
+
 class C2: pass
 class C3: pass
 
 class C1(C2, C3):
-    def setname(self, who):
+    def __init__(self, who):
         self.name = who
 
-user1 = C1()
-user2 = C1()
-
-
-user1.setname('Станислав')
-user2.setname('Савик')
-
-print(user1.name)
-print(user2.name)
