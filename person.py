6b033116d8ef0931f5f3ec5759040885cbbe69fb
ARMOUSE <admin@proger.org>
2017-02-03 19:41:51 +0300

    last version

--- person.py
+++ person.py
@@ -1,74 +1,36 @@
-class Person:
-    def __init__(self, name, pay=0, job=None):
+from classtools import AttrDisplay
+
+class Person(AttrDisplay):
+    """
+    Создает и обрабатывает записи с информацией о людях
+    """
+    def __init__(self, name, job=None, pay=0):
         self.name = name
         self.job = job
         self.pay = pay
-
     def lastName(self):
         return self.name.split()[-1]
-
     def giveRaise(self, percent):
-        self.pay = int(self.pay * (1 + percent))
-
-    def __str__(self):
-        return '[Person: %s, %s]' % (self.name, self.pay)
-
+        self.pay = int(self.pay * (1+percent))
 
 class Manager(Person):
+    """
+    Версия класса Person, адаптированая в соответсвии
+    со специальными требованиями
+    """
     def __init__(self, name, pay):
-        Person.__init__(self, name, pay, job='mgr')
-    # def giveRaise(self, percent, bonus=.10):
-    #     Person.giveRaise(self, percent+bonus)
+        Person.__init__(self, name, 'mgr', pay)
+    def giveRaise(self, percent, bonus=.10):
+        Person.giveRaise(self, percent + bonus)
 
-
-
-class Department:
-    def __init__(self, *args):
-        self.members = list(args)
-    def addMember(self, person):
-        self.members.append(person)
-    def giveRaises(self, percent):
-        for person in self.members:
-            person.giveRaise(percent)
-    def showAll(self):
-        for person in self.members:
-            print(person)
-
-
-bob = Person('Bob Smith')
-sue = Person('Sue Jones', job='dev', pay=100000)
-tom = Person('Tom Beners', pay=50000)
-
-development = Department(bob,sue)
-development.addMember(tom)
-development.giveRaises(.10)
-development.showAll()
-
-
-
-
-# if __name__ == '__main__':
-#     bob = Person('Bob Smith')
-#     sue = Person('Sue Jones', job='dev', pay=100000)
-#     tod = Person('Tod Beners', pay=50000)
-#     print('--All three--')
-#     for object in (bob, sue, tod):
-#         object.giveRaise(.10)
-#         print(object)
-#
-#     print(tod.job)
-
-
-
-
-
-    # print(bob)
-    # print(sue)
-    # print(bob.lastName(), sue.lastName())
-    # sue.giveRaise(.10)
-    # print(sue.pay)
-    # print(sue)
-    # tod.giveRaise(.10)
-    # print(tod.lastName())
-    # print(tod)
-
+if __name__ == '__main__':
+    bob = Person('Bob Smith')
+    sue = Person('Sue Jones', job='dev', pay=10000)
+    tom = Manager('Tom Jones', 50000)
+    print(bob)
+    print(sue)
+    print(bob.lastName(), sue.lastName())
+    sue.giveRaise(.10)
+    print(sue)
+    print(tom.lastName())
+    print(tom)
